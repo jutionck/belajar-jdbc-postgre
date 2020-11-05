73bca783063837ef6be916a0d12c4b4b42f098a3
Jution Candra Kirana <jutionck@gmail.com>
2020-11-05 11:38:05 +0700

    Firt Commit

---
+++ src/com/jdbcpostgre/connectionDb.java
@@ -0,0 +1,29 @@
+package com.jdbcpostgre;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class connectionDb {
+
+    public static void main(String[] args) {
+
+        // auto close connection
+        try (Connection conn = DriverManager.getConnection(
+                "jdbc:postgresql://127.0.0.1:5432/db_manajemenkeuangan", "postgres", "12345qwe")) {
+
+            if (conn != null) {
+                System.out.println("Connected to the database!");
+            } else {
+                System.out.println("Failed to make connection!");
+            }
+
+        } catch (SQLException e) {
+            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
